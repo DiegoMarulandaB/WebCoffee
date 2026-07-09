404ddead21711e72e0afbd0a6581c9c0d3aa64c3
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2026-07-09 13:41:50 -0500

    refactor: update connect-src in Content Security Policy for correct backend URL

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const ContentSecurityPolicy = `
   style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
   img-src 'self' data: blob: https: http:;
   font-src 'self' https://fonts.gstatic.com data:;
-  connect-src 'self' https://cafe-cafe.onrender.com;
+  connect-src 'self' https://webcoffe-backend.onrender.com;
   frame-src 'self';
   object-src 'none';
   base-uri 'self';
