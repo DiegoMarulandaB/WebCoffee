b72a175c94dcf74541e5ace2bef3d3b1051f623b
Diego Marulanda B. <56690521+DiegoMarulandaB@users.noreply.github.com>
2026-07-09 17:06:42 -0500

    fix: update copyright year in footer to 2024

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -33,7 +33,7 @@ const currentYear = new Date().getFullYear();
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <p className="text-sm font-normal">
-            &copy;&nbsp;2023-{currentYear} {""}
+            &copy;&nbsp;2024-{currentYear} {""}
             <span>WebCoffe {""}</span>
           </p>
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-black sm:mb-0 dark:text-white ">
